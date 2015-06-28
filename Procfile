049e0479f163027ed248285406d631c623bfe23f
eduardotrzan <eduardotrzan@gmail.com>
2015-06-28 01:41:12 -0400

    Fixed web runner

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java $JAVA_OPTS -jar -Dlocal.server.port=$PORT target/original-AppChallenge.jar
+web:    java $JAVA_OPTS -jar target/dependency/webapp-runner-7.0.57.2.jar --port $PORT target/appchallenge-1.0.0.war
--- src/main/java/ca/appdirect/appchallenge/AppChallengeApplication.java
+++ src/main/java/ca/appdirect/appchallenge/AppChallengeApplication.java
@@ -1,11 +1,15 @@
 package ca.appdirect.appchallenge;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class AppChallengeApplication extends SpringBootServletInitializer {
 
 	@Override
